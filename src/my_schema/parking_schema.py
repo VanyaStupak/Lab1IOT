b20a5e635af187f2f9177badd2ca539c46605d77
Ivan Stupak <59677896+VanyaStupak@users.noreply.github.com>
2025-03-30 22:57:36 +0200

    Delete src directory

--- src/my_schema/parking_schema.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import Schema, fields
-from domain.gps import Gps
-from my_schema.gps_schema import GpsSchema
-
-class ParkingSchema(Schema):
-    free_parking = fields.Boolean()
-    gps = fields.Nested(GpsSchema)
